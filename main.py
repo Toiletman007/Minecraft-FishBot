27649210fe83f8f0e44f6696612dbc0fcf3ff8e9
Toiletman007 <138164750+Toiletman007@users.noreply.github.com>
2023-09-19 17:29:01 +0200

    Update main.py
    Fixed "Image not foundL" even tho you have the game open

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ screenWidth, screenHeight = pyautogui.size()
 x=0
 z=0
 y: int=0
+notLocated: int = 0
 #Definitions!!!
 
 def find_itL(images, clicks, off_x=0, off_y=0):
     position = pt.locateCenterOnScreen(images, confidence=.75)
     if position is None:
         print('image not foundL')
-
+        notLocated = 1
 
         return 0
     else:
@@ -24,7 +25,7 @@ def find_itL(images, clicks, off_x=0, off_y=0):
 
 # Fishing
 def finditR2(images, clicks, off_x=0, off_y=0):
-    position = pt.locateCenterOnScreen(images, confidence=.50)
+    position = pt.locateCenterOnScreen(images, confidence=.55)
     if position is None:
         print('image not foundR2')
         return 0
@@ -32,9 +33,6 @@ def finditR2(images, clicks, off_x=0, off_y=0):
         global y
         print('Find Fish successful!')
         y = y + 1
-        pt.moveTo(position, duration=.1)
-        pt.moveRel(off_x, off_y, duration=.1)
-
         pt.click(button='right', clicks=clicks, interval=.3)
         print(y, "/", z)
         sleep(.5)
@@ -46,15 +44,19 @@ def Inptake():
     print("Celkem: ", z)
 
 def change_text():
-    pakito["text"] = "Working..."
+    pakito['text'] = "Working..."
     try:
         print('3')
         sleep(1)
         print('2')
         sleep(1)
         print('1')
-
         find_itL('images/StartV2.png', 2)
+        sleep(.5)
+        while(notLocated == 1):
+            sleep(.5)
+            find_itL('images/StartV2.png', 2)
+
         sleep(0.5)
         #First right click for the fishing to start
         pt.click(button='right')
@@ -76,12 +78,8 @@ def change_text():
         ladygaga = customtkinter.CTkLabel(gugu, text="Please input numbers only", text_color="red", text_font=("Comic Sans MS", 10))
         ladygaga.pack()
         gugu.mainloop()
-
-#def change text():
-
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #Window
-
 window = customtkinter.CTk()
 window.title("F.b.")
 window.geometry('1x1+20+200')
@@ -89,31 +87,24 @@ window.minsize(260, 260)
 window.iconbitmap('icons/Fish.ico')
 window.resizable(False, False)
 
-
 grug = customtkinter.CTkLabel(window, text="Hold your Fishing Rod!", text_color="red", text_font=("Bebas Neue", 14))
 grug.pack()
 
-
 text2 = customtkinter.CTkLabel(window, text="Number of Fish: ", bg="#3c3b3c", fg=("#FF813E"))
 text2.pack()
-
 
 elpepe = customtkinter.CTkEntry(width=69, bg="#5A5A5A", fg="#FF7420")
 elpepe.config()
 elpepe.pack(pady=5)
 elpepe.get()
 
-
 pakito2 = customtkinter.CTkButton(text="Save Input", bg="#3c3b3c", command=Inptake)
 pakito2.pack(pady=3.5)
-
 
 pakito = customtkinter.CTkButton(fg_color="#E7A642", hover_color="#FF0000", text="Start!", bg="#E7A642", command=change_text)
 pakito.pack(pady=3.5)
 
-
 text1 = customtkinter.CTkLabel(window, text="Made by Toiletman", bg="#3c3b3c", relief="sunken") #tkinter.Label , bg fg text , Font size type(Bold italic)
 text1.pack(pady=15)
-
 
 window.mainloop()    #Cycle
